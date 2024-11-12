df71bedb63cd9e2048414e5bc98ca7a205c4596c
anjalisridhar <anjalisridhar@anjalisridhar-macbookpro2.roam.internal>
2024-11-12 12:50:29 -0800

    Applying change for deddedc0-7cf8-4a94-8e37-4ab4c7a26204

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
@@ -56,9 +56,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
 		configurer
-				.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret))
-						.authorizedGrantTypes(grantType).scopes(scopeRead, scopeWrite)
-						.resourceIds(resourceIds);
+				.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret)).authorizedGrantTypes(grantType)
+						.scopes(scopeRead, scopeWrite).resourceIds(resourceIds).accessTokenValiditySeconds(1800);
 	}
 
 	@Override
