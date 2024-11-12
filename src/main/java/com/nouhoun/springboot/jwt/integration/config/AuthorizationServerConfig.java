251c1269d75a0dba84f3e23675d2a39471cb9bfc
anjalisridhar <anjalisridhar@anjalisridhar-macbookpro2.roam.internal>
2024-11-12 12:52:41 -0800

    Applying change for 31236440-09c7-4c0d-a20f-9501adb33502

--- src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/config/AuthorizationServerConfig.java
@@ -56,8 +56,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
 		configurer
-				.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret)).authorizedGrantTypes(grantType).scopes(scopeRead, scopeWrite)
-						.resourceIds(resourceIds).accessTokenValiditySeconds(3600).refreshTokenValiditySeconds(2592000);
+				.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret)).authorizedGrantTypes(grantType).scopes(scopeRead, scopeWrite).resourceIds(resourceIds)
+						.accessTokenValiditySeconds(24*60*60).refreshTokenValiditySeconds(6*24*60*60);
 	}
 
 	@Override
