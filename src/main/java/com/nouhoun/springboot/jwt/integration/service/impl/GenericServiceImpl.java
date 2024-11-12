7efeb9c5a77a63676d20a1bb7c400b4f8e6d9751
anjalisridhar <anjalisridhar@anjalisridhar-macbookpro2.roam.internal>
2024-11-12 12:48:43 -0800

    Applying change for a6952fd3-97a6-40fd-8896-34bad869e01b

--- src/main/java/com/nouhoun/springboot/jwt/integration/service/impl/GenericServiceImpl.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/service/impl/GenericServiceImpl.java
@@ -28,11 +28,11 @@ public class GenericServiceImpl implements GenericService {
 
     @Override
     public List<User> findAllUsers() {
-        return (List<User>)userRepository.findAll();
+        return (List<User>) userRepository.findAll();
     }
 
     @Override
     public List<RandomCity> findAllRandomCities() {
-        return (List<RandomCity>)randomCityRepository.findAll();
+        return (List<RandomCity>) randomCityRepository.findAll();
     }
 }
