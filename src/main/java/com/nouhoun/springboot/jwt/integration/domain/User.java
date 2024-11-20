0dbb707806c32f42184c7f09d30ffa51e6d686b0
anjalisridhar <anjalisridhar@anjalisridhar-macbookpro2.roam.internal>
2024-11-20 14:03:17 -0800

    Applying fix error for anj/demo-mig-2-recursing_goldstine and fix error.

--- src/main/java/com/nouhoun/springboot/jwt/integration/domain/User.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/domain/User.java
@@ -3,7 +3,7 @@ package com.nouhoun.springboot.jwt.integration.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotEmpty;
+import jakarta.validation.constraints.NotEmpty;
 
 import javax.persistence.*;
 import java.util.List;
@@ -46,4 +46,3 @@ public class User {
                     referencedColumnName = "id"))
     private List<Role> roles;
 }
-
