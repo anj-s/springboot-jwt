5aabd0e59de0fa2c39b063b3177c25ac08901a85
anjalisridhar <anjalisridhar@anjalisridhar-macbookpro2.roam.internal>
2025-01-12 08:28:29 -0800

    Applying fix error for anj/e2e-eval-nervous-robinson-fix-error-87a41a0e and fix error.

--- src/main/java/com/nouhoun/springboot/jwt/integration/service/impl/AppUserDetailsService.java
+++ src/main/java/com/nouhoun/springboot/jwt/integration/service/impl/AppUserDetailsService.java
@@ -24,19 +24,13 @@ public class AppUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(s);
-27 |
         if(user == null) {
             throw new UsernameNotFoundException(String.format("The username %s doesn't exist", s));
         }
-31 |
         List<GrantedAuthority> authorities = new ArrayList<>();
         user.getRoles().forEach(role -> {
             authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
         });
-
-        UserDetails userDetails = new org.springframework.security.core.userdetails.
-                User(user.getUsername(), user.getPassword(), authorities);
-39 |
         return userDetails;
     }
 }
